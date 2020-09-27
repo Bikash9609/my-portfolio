37fc4bf3f031055cc706fed349a65afd7c545532
Bikash <bikash@localhost.localdomain>
2020-09-27 10:54:29 +0530

    Added build

---
+++ build/precache-manifest.688075db5a76982f21902551a1f4e33e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54711e846e7d56c6c92978814a49c6dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da1f49a765aead38e150",
+    "url": "/static/css/main.9516d568.chunk.css"
+  },
+  {
+    "revision": "0efcf7dec308d97ea847",
+    "url": "/static/js/2.84af8196.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.84af8196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1f49a765aead38e150",
+    "url": "/static/js/main.e417ee40.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a2bf0a10a1b3bfb9cfc38db18ddf8a95",
+    "url": "/static/media/Bootstrap_(front-end_framework)-Logo.wine.a2bf0a10.svg"
+  },
+  {
+    "revision": "b6cc70f663088a873fe748e5224134ea",
+    "url": "/static/media/Coffee-Day-Coffee-Shop.b6cc70f6.png"
+  },
+  {
+    "revision": "9b436191c874a5cdbbd34224cbb1d405",
+    "url": "/static/media/DSPMU-Official.9b436191.png"
+  },
+  {
+    "revision": "6caffb863dcd0c31ad2ff7ade6be19f5",
+    "url": "/static/media/Home (1).6caffb86.png"
+  },
+  {
+    "revision": "bc617a57289c429961144a644e655195",
+    "url": "/static/media/PinkShirt.bc617a57.png"
+  },
+  {
+    "revision": "c0c3380f55e4c7530de2e390e8e133e7",
+    "url": "/static/media/React-App-Mobile.c0c3380f.png"
+  },
+  {
+    "revision": "265b3b120d6b464eeb364ca5c62dfb13",
+    "url": "/static/media/React-App.265b3b12.png"
+  },
+  {
+    "revision": "b636c5c01430e8642b3997073fd8eadb",
+    "url": "/static/media/Sign-In.b636c5c0.png"
+  },
+  {
+    "revision": "6833d64cb80cc1287d64ddcb5a32011d",
+    "url": "/static/media/Title.6833d64c.png"
+  },
+  {
+    "revision": "76472f0aa83a4f80157419707555e2ba",
+    "url": "/static/media/material-ui-logo-png-transparent.76472f0a.png"
+  },
+  {
+    "revision": "77559a8e9979c11bb14cae9fcd74f6aa",
+    "url": "/static/media/nodejs-expressjs-javascript-solution-stack-web-application-expressjs-png-800_490.77559a8e.jpg"
+  },
+  {
+    "revision": "979b6a2da9983a402c11396a875f9bc4",
+    "url": "/static/media/nodejs-javascript-web-application-expressjs-computer-software-node-png-820_820.979b6a2d.jpg"
+  }
+]);
